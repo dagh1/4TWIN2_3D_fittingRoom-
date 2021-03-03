c921fb319793d24f5ba9cd238f1fc41306a4e1dc
YassineBay <yassine.bayoudh@outlook.com>
2021-03-03 23:00:40 +0100

    Please Work

--- easyWearBackEnd/routes/photos.js
+++ easyWearBackEnd/routes/photos.js
@@ -1,41 +1,28 @@
-var express = require('express');
-const Photo = require('../models/Photo');
+var express = require("express");
+const Photo = require("../models/Photo");
 var router = express.Router();
 
-
-router.get('/', function(req, res, next) {
-Photo.find(
-(err, photo) => {
-res.render(
-'form.twig',
-{
-title : "Contact list",
-cont : photo
-}
-);
-}
-);
-
+router.get("/", function (req, res, next) {
+  Photo.find((err, photo) => {
+    res.render("form.twig", {
+      title: "Contact list",
+      cont: photo,
+    });
+  });
 });
-router.post('/add', function(req, res, next) {
+router.post("/add", function (req, res, next) {
   new Photo({
-  
-  url: req.body.url,
-  description: req.body.description,
-  datecreation: req.body.datecreation,
-  })
-  .save(
-  (err,newcontact)=>{
-  if (err)
-  console.log("error message : "+err);
-  else{
-  console.log(newcontact);
-  
-  res.json(" : photo :" + newcontact._id +" added");
-  
-  }
-  });}
-  );
+    url: req.body.url,
+    description: req.body.description,
+    datecreation: req.body.datecreation,
+  }).save((err, newcontact) => {
+    if (err) console.log("error message : " + err);
+    else {
+      console.log(newcontact);
 
+      res.json(" : Please work  :" + newcontact._id + " added");
+    }
+  });
+});
 
 module.exports = router;
